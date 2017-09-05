6af48f01683af948a1338498db66b1082c568cf1
Paweł Idziak <pa.idziak@gmail.com>
2017-09-05 18:01:28 +0200

    Dodanie kontrolera wydarzeń (Evento.Api / Controllers / EventsController.cs)

---
+++ src/Evento.Api/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Evento.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evento.Api.Controllers
+{
+    [Route("[controller]")]
+    public class EventsController : Controller
+    {
+
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string name)
+        {
+            var events = await _eventService.BrowseAsyns(name);
+            return Json(events);
+        }
+    }
+}
